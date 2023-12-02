6b39bc5a2a9d144629dc509e310036abb939f0d7
muntrn76 <tranxuantruc2101@gmail.com>
2023-12-02 11:13:41 +0700

    create view data collection page Signed-off-by: muntrn76 <tranxuantruc2101@gmail.com>

--- collection_page/collection.php
+++ collection_page/collection.php
@@ -6,42 +6,120 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="undefined" crossorigin="anonymous">
+        integrity="undefined" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="undefined"
-    crossorigin="anonymous"></script>
+          crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="../component/navbar.css">
 </head>
 
-<body >
-  <?php include("../component/navbar.html"); ?>
-  <div class="container d-flex flex-column align-items-center">
-    <table class="table border-5 rounded-3 overflow-hidden ">
-      <colgroup>
-        <col span="1" style="width: 5%;">
-        <col span="1" style="width: 70%;">
-        <col span="1" style="width: 25%;">
-      </colgroup>
-      <thead>
-        <tr>
-          <th scope="col">
-            <p class="text-primary mb-2">#</p>
-          </th>
-          <th scope="col">
-            <p class="text-primary mb-2">CV Title</p>
-          </th>
-          <th scope="col">
-            <p class="text-primary mb-2">Last Modified</p>
-          </th>
-        </tr>
-      </thead>
-      <tbody id="tableBody">
-      </tbody>
-    </table>
-    <button type="button" class="btn btn-primary">Show more</button>
-  </div>
-  <script src="addTable.js" defer></script>
+<body>
+<?php
+include("../component/navbar.html");
+include '../config/db.php'; // Kết nối đến cơ sở dữ liệu
+
+// Lấy danh sách hồ sơ từ cơ sở dữ liệu
+$resumes = mysqli_query($conn, "SELECT * FROM resumes");
+?>
+<div class="container d-flex flex-column align-items-center" id="resumeContainer">
+  <table class="table border-5 rounded-3 overflow-hidden">
+    <colgroup>
+      <col span="1" style="width: 5%;">
+      <col span="1" style="width: 70%;">
+      <col span="1" style="width: 25%;">
+    </colgroup>
+    <thead>
+    <tr>
+      <th scope="col">
+        <p class="text-primary mb-2">#</p>
+      </th>
+      <th scope="col">
+        <p class="text-primary mb-2">CV Title</p>
+      </th>
+      <th scope="col">
+        <p class="text-primary mb-2">Last Modified</p>
+      </th>
+    </tr>
+    </thead>
+    <tbody class="table-border-bottom-0" id="resumeTableBody">
+    <!-- Hiển thị 5 sản phẩm ban đầu -->
+    <?php
+    $limit = 5;
+    $query = "SELECT * FROM resumes LIMIT $limit";
+    $resumes = mysqli_query($conn, $query);
+
+    while ($row = mysqli_fetch_assoc($resumes)) :
+      ?>
+      <tr>
+        <td><?php echo $row['id']; ?></td>
+        <td><?php echo $row['title']; ?></td>
+        <td><?php echo $row['update_at']; ?></td>
+      </tr>
+    <?php endwhile; ?>
+    </tbody>
+  </table>
+  <button type="button" class="btn btn-primary" id="loadMoreBtn">Show more</button>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+  $(document).ready(function () {
+    var limit = 5; // Số sản phẩm được tải thêm mỗi lần
+    var offset = limit; // Bắt đầu từ vị trí đầu tiên
+    var searchTerm = ''; // Biến theo dõi giá trị tìm kiếm
+
+    // Code AJAX để tìm kiếm theo title và load thêm
+    function loadResults() {
+      $.ajax({
+        type: "POST",
+        url: "load-more.php",
+        data: { searchTerm: searchTerm, limit: limit, offset: offset },
+        success: function (response) {
+          $("#resumeTableBody").append(response);
+          offset += limit;
+          toggleShowMoreButton(); // Cập nhật trạng thái nút "Show more"
+        }
+      });
+    }
+
+    // Code AJAX để tìm kiếm theo title
+    $("#searchForm").submit(function (e) {
+      e.preventDefault();
+      searchTerm = $("#searchInput").val();
+      offset = 0; // Reset lại offset khi tìm kiếm mới và bắt đầu từ vị trí đầu tiên
+      $.ajax({
+        type: "POST",
+        url: "search.php",
+        data: { searchTerm: searchTerm, limit: limit, offset: offset },
+        success: function (response) {
+          $("#resumeTableBody").html(response);
+          offset += limit;
+          toggleShowMoreButton(); // Cập nhật trạng thái nút "Show more"
+        }
+      });
+    });
+
+    // Code AJAX để tải thêm sản phẩm khi không có tìm kiếm
+    $("#loadMoreBtn").click(function () {
+      loadResults();
+    });
+
+    // Hàm cập nhật trạng thái nút "Show more" dựa trên offset và trạng thái tìm kiếm
+    function toggleShowMoreButton() {
+      var totalResults = $("#resumeTableBody tr").length; // Đếm số lượng kết quả hiện tại
+      if (offset <= totalResults) {
+        $("#loadMoreBtn").show();
+      } else {
+        $("#loadMoreBtn").hide();
+      }
+    }
+  });
+</script>
+
+
+</div>
+<!--<script src="addTable.js" defer></script>-->
 </body>
 
 </html>
