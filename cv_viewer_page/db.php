0833214d13f5a825611805159cc6b8d7d097910c
vinhdao1006 <koncakbo@gmail.com>
2023-12-03 03:09:47 +0700

    update full cv viewer

---
+++ cv_viewer_page/db.php
@@ -0,0 +1,140 @@
+<?php 
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cv_information";
+
+    // create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // check connection
+    if($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    //update password hashed
+    $sql = "SELECT id, password FROM users";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $userId = $row['id'];
+            $plainPassword = $row['password'];
+
+            // Hash the password
+            $hashedPassword = password_hash($plainPassword, PASSWORD_DEFAULT);
+
+            // Update the database with the hashed password
+            $updateSql = "UPDATE users SET password = '$hashedPassword' WHERE id = $userId";
+            $conn->query($updateSql);
+        }
+        // echo "Passwords hashed and updated successfully.";
+    } else {
+        echo "No users found.";
+    }
+
+
+    //retrieve data
+    $user_id = 2;
+
+    //user
+    $sql_user = "SELECT * FROM users WHERE id = $user_id";
+    $result_user = $conn->query($sql_user);
+
+    if ($result_user->num_rows > 0) {
+        $user = $result_user->fetch_assoc();
+    } else {
+        echo "User not found.";
+    }
+
+    //resume
+    $sql_resume = "SELECT id FROM resumes WHERE user_id = $user_id";
+    $result_resume = $conn->query($sql_resume);
+
+    if ($result_resume->num_rows > 0) {
+        while ($row = $result_resume->fetch_assoc()) {
+            $resume_id = $row["id"];
+
+        }
+    } else {
+        echo "Resume not found.";
+    } 
+
+    //education
+    $sql_edu = "SELECT * FROM education WHERE id = $user_id";
+    $result_edu = $conn->query($sql_edu);
+
+    if($result_edu->num_rows > 0) {
+        $edu = $result_edu->fetch_assoc();
+    } else {
+        echo "Education not found.";
+    }
+
+    //experiences
+    function getExperiences($resume_id, $conn) {
+        $sql_exp = "SELECT * FROM experiences WHERE resume_id = $resume_id";
+        // echo "Resume ID for User: $resume_id";
+        $result_exp = $conn->query($sql_exp);
+
+        // Check if there are any experiences
+        if ($result_exp->num_rows > 0) {
+            $experiences = array();
+
+            // Iterate over the result set and add each experience to the array
+            while ($exp = $result_exp->fetch_assoc()) {
+                $experiences[] = $exp;
+            }
+
+            return $experiences;
+        } else {
+            return array(); // Return an empty array if no experiences are found
+        }
+    }
+
+    //skills
+    function getSkills($resume_id, $conn) {
+        $sql_skill = "SELECT * FROM skills WHERE resume_id = $resume_id";
+        $result_skill = $conn->query($sql_skill);
+
+        // Check if there are any experiences
+        if ($result_skill->num_rows > 0) {
+            $skills = array();
+
+            // Iterate over the result set and add each experience to the array
+            while ($skill = $result_skill->fetch_assoc()) {
+                $skills[] = $skill;
+            }
+
+            return $skills;
+        } else {
+            return array(); // Return an empty array if no experiences are found
+        }
+    }
+
+    //projects
+    function getProjects($resume_id, $conn) {
+        $sql_project = "SELECT * FROM projects WHERE resume_id = $resume_id";
+        $result_project = $conn->query($sql_project);
+
+        // Check if there are any experiences
+        if ($result_project->num_rows > 0) {
+            $projects = array();
+
+            // Iterate over the result set and add each experience to the array
+            while ($project = $result_project->fetch_assoc()) {
+                $projects[] = $project;
+            }
+
+            return $projects;
+        } else {
+            return array(); // Return an empty array if no experiences are found
+        }
+    }
+
+    
+
+    // Close connection
+    // $conn->close();
+
+?>
---
+++ data.sql
@@ -0,0 +1,160 @@
+CREATE DATABASE cv_information;
+
+USE cv_information;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    gender VARCHAR(255),
+    address VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(20),
+    password VARCHAR(255),
+    dob date,
+    position VARCHAR(255),
+    introduction TEXT
+);
+
+CREATE TABLE avatar (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+    image LONGBLOB,
+    resume_id INT(11) 
+);
+
+CREATE TABLE resumes (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_id INT(11) 
+);
+
+CREATE TABLE projects (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    duration INT(11),
+    web VARCHAR(255),
+    field VARCHAR(255),
+    description TEXT,
+    technology VARCHAR(255),
+    resume_id INT(11) 
+);
+
+CREATE TABLE education (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    uni_name VARCHAR(255),
+    degree VARCHAR(255),
+    major VARCHAR(255),
+    grad_year INT(4),
+    resume_id INT(11) 
+);
+
+CREATE TABLE skills (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    resume_id INT(11) 
+);
+
+CREATE TABLE experiences (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    duration INT(11),
+    position VARCHAR(255),
+    company VARCHAR(255),
+    resume_id INT(11) 
+);
+
+-- ADD FOREIGN KEYS
+
+ALTER TABLE resumes
+    ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ 
+ALTER TABLE avatar 
+    ADD FOREIGN KEY (resume_id) REFERENCES resumes(id);
+
+ALTER TABLE projects
+    ADD FOREIGN KEY (resume_id) REFERENCES resumes(id);
+
+ALTER TABLE education
+    ADD FOREIGN KEY (resume_id) REFERENCES resumes(id);
+
+ALTER TABLE skills
+    ADD FOREIGN KEY (resume_id) REFERENCES resumes(id);
+
+ALTER TABLE experiences
+    ADD FOREIGN KEY (resume_id) REFERENCES resumes(id);
+
+
+
+-- INSERT USER 
+INSERT INTO users (name, gender, address, email, phone, password, dob, position, introduction) VALUES 
+('VINH DAO QUANG', 'Male', 'HCMC', 'vinhdao@gmail.com', '0909002123', 'truetruetrue', '2002-06-10', 'Front-end Developer',
+'I am junior in Computer Science at Ho Chi Minh University of Technology. As a self-awareness and self-discipline person, I am able to handle my jobs at a considerate level. 
+To be honest, I am really dedicated to improve myself and I am keen on coding, especially with Javascript. I like to work with team because I am willing to listen every comments which is highly contributed to projects as well as myself. In addition, I am able to conduct research and learn new knowledge in a short period (usually in about two weeks) as well as learning from persons who are likely to make me closer to perfection.
+To tell about my further wishes, working as a software developer in about 5 - 8 years will be enough for me before deciding to face new challenges in coding and creating applications in both mobile and web field.');
+
+INSERT INTO users (name, gender, address, email, phone, password, dob, position, introduction) VALUES 
+('JOHN SMITH', 'Male', 'London', 'johnsmith@gmail.com', '091999323', 'sadness', '1988-02-03', 'Fullstack Developer',
+'I am junior in Computer Science at Standford University. As a self-awareness and self-discipline person, I am able to handle my jobs at a considerate level. 
+To be honest, I am really dedicated to improve myself and I am keen on coding, especially with Javascript. I like to work with team because I am willing to listen every comments which is highly contributed to projects as well as myself. In addition, I am able to conduct research and learn new knowledge in a short period (usually in about two weeks) as well as learning from persons who are likely to make me closer to perfection.
+To tell about my further wishes, working as a software developer in about 5 - 8 years will be enough for me before deciding to face new challenges in coding and creating applications in both mobile and web field.');
+
+-- INSERT RESUME
+INSERT INTO resumes (title, created_at, update_at, user_id) VALUES 
+('Application for web developers', '2023-12-01', '2023-12-02', 1); -- Vinh Dao Quang
+
+INSERT INTO resumes (title, created_at, update_at, user_id) VALUES 
+('Application for fullstack developers', '2023-12-01', '2023-12-03', 2); -- John Smith
+
+-- INSERT PROJECTS
+INSERT INTO projects (name, duration, web, technology, field, description, resume_id) VALUES 
+('Alimac Shopping - Shirt shopping website', '3', 'www.alimac.com', 'ReactJs, NextJs, Bootstrap, VueJS', 'Front-end, Back-end', 'I am OK, I am fine, quenchana, teng neng neng neng neng', 1);
+
+INSERT INTO projects (name, duration, web, technology, field, description, resume_id) VALUES 
+('Cat Tinder - Where cats can find true love', '3', 'www.cattinder.com', 'ReactJs, NextJs, Bootstrap, VueJS', 'Front-end, Back-end', 'I am OK, I am fine, quenchana, teng neng neng neng neng', 1);
+
+INSERT INTO projects (name, duration, web, technology, field, description, resume_id) VALUES 
+('Travel No Price - Booking tickets website', '3', 'www.travelnoprice.com', 'ReactJs, NextJs, Bootstrap, VueJS', 'Front-end, Back-end', 'I am OK, I am fine, quenchana, teng neng neng neng neng', 1);
+
+INSERT INTO projects (name, duration, web, technology, field, description, resume_id) VALUES 
+('Travel No Price - Booking tickets website', '4','www.travelnoprice.com', 'ReactJs, NextJs, Bootstrap, VueJS', 'Front-end, Back-end', 'I am OK, I am fine, quenchana, teng neng neng neng neng, raham sakit', 2);
+
+INSERT INTO projects (name, duration, web, technology, field, description, resume_id) VALUES 
+('Alimac Shopping - Shirt shopping website', '3', 'www.alimac.com', 'ReactJs, NextJs, Bootstrap, VueJS', 'Front-end, Back-end', 'I am OK, I am fine, quenchana, teng neng neng neng neng', 2);
+
+INSERT INTO projects (name, duration, web, technology, field, description, resume_id) VALUES 
+('Cat Tinder - Where cats can find true love', '3', 'www.cattinder.com', 'ReactJs, NextJs, Bootstrap, VueJS', 'Front-end, Back-end', 'I am OK, I am fine, quenchana, teng neng neng neng neng', 2);
+
+-- INSERT SKILLS
+INSERT INTO skills (name, resume_id) VALUES ('JavaScript', 1);
+INSERT INTO skills (name, resume_id) VALUES ('C++', 1);
+INSERT INTO skills (name, resume_id) VALUES ('PHP', 1);
+INSERT INTO skills (name, resume_id) VALUES ('Python', 1);
+
+INSERT INTO skills (name, resume_id) VALUES ('PHP', 2);
+INSERT INTO skills (name, resume_id) VALUES ('JavaScript', 2);
+INSERT INTO skills (name, resume_id) VALUES ('C#', 2);
+INSERT INTO skills (name, resume_id) VALUES ('Ruby', 2);
+
+-- INSERT EDUCATION
+INSERT INTO education (uni_name, degree, major, grad_year, resume_id) VALUES 
+(' Hochiminh University of Technology', 'Bachelor', 'Computer Science', '2020', 1);
+
+INSERT INTO education (uni_name, degree, major, grad_year, resume_id) VALUES 
+('Standford University', 'Master', 'Computer Engineering', '2010', 2);
+
+-- INSERT EXPERIENCE
+INSERT INTO experiences (duration, position, company, resume_id) VALUES 
+('24', 'Senior Front-end Developer', 'Bee Solution Ltd.', 1);
+
+INSERT INTO experiences (duration, position, company, resume_id) VALUES 
+('12', 'Senior Front-end Developer', 'VNG', 1);
+
+INSERT INTO experiences (duration, position, company, resume_id) VALUES 
+('36', 'Senior Back-end Developer', 'Google', 2);
+
+INSERT INTO experiences (duration, position, company, resume_id) VALUES 
+('24', 'Senior Fullstack Developer', 'Youtube', 2);
+
+
+
+
